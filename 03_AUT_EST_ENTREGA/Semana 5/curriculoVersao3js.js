a8414a24d9878b0679fd120a71cb5a4ecd7088ae
taiinteli <99188288+taiinteli@users.noreply.github.com>
2022-05-28 09:43:20 -0300

    curriculoVersao004

--- 03_AUT_EST_ENTREGA/Semana 5/curriculoVersao3js.js
+++
@@ -1,6 +0,0 @@
-function adicionarInformacoes() {
-    var inputText = document.getElementById('conquistas').value
-    var box = document.getElementById('box')
-    
-    box.innerHTML += inputText
-}
