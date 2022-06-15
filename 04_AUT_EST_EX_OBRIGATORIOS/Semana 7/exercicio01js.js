05e8bfef386fd766f3550d55508be74a02c2402f
taiinteli <99188288+taiinteli@users.noreply.github.com>
2022-06-15 13:01:13 -0300

    updateexercicio01

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/exercicio01js.js
@@ -0,0 +1,15 @@
+function Calcular(){
+     var numero = Number(document.getElementById("text1").value)
+     numero = numero + 1 
+
+     document.getElementById("text1").value = String(numero)  
+ 
+}
+
+function Calcular2() {
+ var numero = Number(document.getElementById("text1").value)
+ numero = numero - 1 
+
+ document.getElementById("text1").value = String(numero)
+
+}
